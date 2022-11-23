84c4bdf0db0795fd8cd7a7d7da1ada7f236b1c5f
pro-soichiro <dr.1987626@gmail.com>
2022-11-23 19:49:04 +0900

    [finish] section12

--- section12/js/main.js
+++ section12/js/main.js
@@ -1,6 +1,13 @@
 const app = Vue.createApp({
   data: () => ({
-
+    message: 'hello vue.js',
+    checked: false,
+    colors: [],
+    radioColor: '',
+    selected: '',
+    items: ['red', 'green', 'blue', 'yellow', 'pink'],
+    multiple: [],
+    text: '',
   }),
 })
 
@@ -10,5 +17,30 @@ app.mount('#app')
 /*
 
 フォーム入力バインディング
+
+# v-modelディレクティブ
+1. データバインディングで、要素のvalue属性を更新する。
+2. イベントハンドリングで、受け取った値をデータに代入する。
+
+v-model使用時はvalue, checked, selectedといった属性は無視される。
+
+textareaタグ内ではマスタッシュ構文が双方向バインディングに対応していないため、
+inputタグなどと同じようにv-modelディレクティブを使用すること
+
+
+フォーム部品を扱うときはデフォルトのデータの型が何かを意識することが大切。
+radio string
+checkbox boolean
+checkboxes array
+select string
+
+
+v-model修飾子
+
+修飾子の種類
+.lazy   バインドのタイミングを遅延させる
+.trim   入力力前後の空白を削除してからデータに代入
+.number  入力値を数値型に型変換してからデータに代入
+
 
 */

97bfeca28e1895535ae3b65b3cbdc378d37f5ead
pro-soichiro <dr.1987626@gmail.com>
2022-11-23 20:23:09 +0900

    [finish] section14

--- section14/js/main.js
+++ section14/js/main.js
@@ -1,6 +1,6 @@
 const app = Vue.createApp({
   data: () => ({
-
+    show: true
   }),
 })
 
@@ -9,6 +9,11 @@ app.mount('#app')
 
 /*
 
+clickイベントで真偽値を反転させるだけであれば
+下記のように記述することができる
+<button v-on:click="show = !show">button</button>
 
+
+transitionタグでvueのトランジションアニメーションを使用することができる
 
 */

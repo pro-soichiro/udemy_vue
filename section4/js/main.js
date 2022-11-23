28af3c129dbc62831f0ed73fa445e66a59de1018
pro-soichiro <dr.1987626@gmail.com>
2022-11-23 11:16:29 +0900

    section4 todoアプリの完成

--- section4/js/main.js
+++ section4/js/main.js
@@ -1,8 +1,39 @@
 
 const app = Vue.createApp({
   data: () => ({
-    test: 'test'
-  })
+    newTodo: '',
+    todos: [],
+  }),
+  methods: {
+    addTodo() {
+      // 下記でもいける
+      // if(this.newTodo === '') return
+      if(this.newTodo !== '') {
+        // オブジェクト形式で追加していくことでタスクの完了、未完了を管理していく
+        let todoObj = {
+          item: this.newTodo,
+          isDone: false,
+        }
+        this.todos.unshift(todoObj)
+        this.newTodo = ''
+      }
+    },
+    deleteTodo(index) {
+      console.log(index)
+      // deleteではなく、spliceメソッドを使用する
+      // delete this.todos[index]
+      this.todos.splice(index, 1)
+
+
+      /*
+      deleteとspliceの違い
+
+      deleteは配列ではなくプロパティの値を削除するため、indexが残ってしまい、
+      意図した挙動となっていなかった
+      */
+    }
+
+  }
 })
 
 app.mount('#app')

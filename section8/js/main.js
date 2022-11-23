42ee6e378a6f866324e1e8495e58b3a55e46ecd3
pro-soichiro <dr.1987626@gmail.com>
2022-11-23 17:05:37 +0900

    [finish] section8

--- section8/js/main.js
+++ section8/js/main.js
@@ -1,14 +1,49 @@
 const app = Vue.createApp({
   data: () => ({
+    items: null,
+    keyword: "",
+    message: "",
   }),
-})
+  watch: {
+    keyword(val) {
+      this.message = 'Waiting for you to stop typing...'
+      this.debouncedGetAnswer()
+    }
+  },
+  methods: {
+    getAnswer() {
+      if (this.keyword === "") {
+        this.items = null;
+        return;
+      }
 
-app.mount('#app')
+      this.message = "Loading...";
+      const vm = this;
+      const params = { page: 1, per_page: 20, query: this.keyword };
+      axios
+        .get("https://qiita.com/api/v2/items", { params })
+        .then((res) => {
+          vm.items = res.data
+        })
+        .catch((e) => {
+          vm.message = 'Error!' + e
+        })
+        .finally(() => {
+          vm.message = ''
+        });
+    },
+  },
+  mounted() {
+    this.keyword = 'JavaSctipt'
+    this.getAnswer()
+    this.debouncedGetAnswer = _.debounce(this.getAnswer, 1000)
+  },
+});
 
+app.mount("#app");
 
 /*
 
 APIを用いたリアルタイムサーチの実装で監視プロパティの理解を深める
-
 
 */

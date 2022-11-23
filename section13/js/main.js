8fb106a6f9c8f6231f5d334a4cf6ade2275d83bc
pro-soichiro <dr.1987626@gmail.com>
2022-11-23 20:08:02 +0900

    [finish] section13

--- section13/js/main.js
+++ section13/js/main.js
@@ -1,8 +1,39 @@
+// ローカルコンポーネント
+const helloComponent = {
+  template: '<p>local hello!</p>'
+}
+
+// 動的なコンポーネント
+const buttonCounter = {
+  template: '<div><span>count: </span><button @click="countUp">{{count}}</button></div>',
+  data: () => ({
+    count: 0,
+  }),
+  methods: {
+    countUp() {
+      this.count ++
+    }
+  }
+}
+
+
+
 const app = Vue.createApp({
   data: () => ({
 
   }),
+  components: {
+    'local-hello-component': helloComponent,
+    'button-counter': buttonCounter,
+  }
 })
+
+// グローバルコンポーネント
+app.component('hello-component', {
+  template: '<p>hello!</p>'
+})
+
+
 
 app.mount('#app')
 
@@ -10,5 +41,15 @@ app.mount('#app')
 /*
 
 コンポーネントの利用方法
+
+
+コンポーネント名の定義ルール
+- ハイフンを一つ以上含むケバブケースを利用しなければならない。
+
+- キャメルケース、スネークケースは許可されていない。
+- 一単語は許可されていない。
+
+理由は、HTMLタグとの衝突を避けるためである。
+
 
 */

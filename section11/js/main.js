ab0f9b671c1378c6aab5f631f54eeb2180b24731
pro-soichiro <dr.1987626@gmail.com>
2022-11-23 18:51:04 +0900

    [finish] section11

--- section11/js/main.js
+++ section11/js/main.js
@@ -1,7 +1,31 @@
 const app = Vue.createApp({
   data: () => ({
-
+    count: 0,
+    date: '00:00:00',
+    dateOnce: '00:00:00',
   }),
+  methods: {
+    countUp() {
+      this.count ++
+    },
+    countEvent(e) {
+      this.count ++
+      e.target.innerHTML = this.count
+    },
+    countUpArg(arg) {
+      this.count += arg
+    },
+    eventAndArg($event, arg) {
+      this.count += arg
+      $event.target.innerHTML = $event.target.id
+    },
+    printDate() {
+      this.date = new Date().toLocaleTimeString()
+    },
+    printDateOnce() {
+      this.dateOnce = new Date().toLocaleTimeString()
+    },
+  }
 })
 
 app.mount('#app')
@@ -10,5 +34,17 @@ app.mount('#app')
 /*
 
 イベントハンドラ
+
+省略記法
+
+
+バインドはコロン
+v-bind:class
+:class
+
+
+イベントはアットマーク
+v-on:click
+@click
 
 */
